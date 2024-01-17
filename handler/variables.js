0407fe03417a27f62f215517d7cfe1c5653860c8
Simvol <144470909+S1mvolxD@users.noreply.github.com>
2024-01-17 21:28:10 +0700

    variables.js
    Beta

---
+++ handler/variables.js
@@ -0,0 +1,21 @@
+module.exports = (client) => {
+  client.variables(
+    {
+      prefix: "t!",
+      color: "#303136",
+      autoplay: "false",
+      music_channelID: "null",
+      music_player: "null",
+      yes_emoji: "<:on:1149159296731447368>",
+      volume: "100",
+      authorbutton: "0",
+      play_pause: "none",
+      loop: "none",
+      songTitle: "",
+      songID: "",
+      songDuration: "",
+      songThumbnail: "",
+    },
+    "main",
+  );
+};
