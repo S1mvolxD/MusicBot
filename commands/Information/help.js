100df53227dd1fe6113ed0be10772213e5c39ba8
Simvol <144470909+S1mvolxD@users.noreply.github.com>
2024-01-17 21:35:31 +0700

    help.js

---
+++ commands/Information/help.js
@@ -0,0 +1,14 @@
+module.exports = [{
+  name: "help",
+  code: `
+  $author[List of commands;$userAvatar[$clientID]]
+
+  $addField[Settings;\`prefix\`;false]
+  $addField[Music;\`play\`, \`skip\`, \`stop\`, \`leave\`, \`join\`, \`volume\`, \`autoplay\`;false]
+  $addField[Information;\`stats\`, \`help\`, \`ping\`;false]
+
+  $color[$getGuildVar[color]]
+  $footer[Used the command ~ $username;$authorAvatar]
+  $addTimestamp
+  `
+}]  
